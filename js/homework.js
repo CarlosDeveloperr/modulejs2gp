fc8332ad62fb8f4e03c5ac1dccea415a70291508
dered-dev <jlcc170@gmail.com>
2021-05-03 22:57:54 -0500

    ADD homework exercises

---
+++ js/homework.js
@@ -0,0 +1,85 @@
+/**
+ * Ejercicios
+ */
+
+/**
+ * 1:
+ * Pedir al usuario estos 3 datos
+ * - Nombre
+ * - Peso actual
+ * Calcular el peso que tendria esa persona en la luna
+ * Imprimirlo en consola
+ * ** Opcional -> Crear una función **
+ */
+
+
+/**
+ * 1:
+ * Pedir al usuario estos 3 datos
+ * - Nombre
+ * - Peso actual
+ * Calcular el peso que tendria esa persona en la luna
+ * Imprimirlo en consola
+ * ** Opcional -> Crear una función **
+ */
+
+
+/**
+ * 2:
+ * Pedir al usuario la calificación de un examen ( 0 a 100)  por el prompt
+ * Imprimir en consola las equivalencias en letra su calificación, es decir
+ * -> si es mayor o igual a 90, imprimir "A"
+ * -> si es mayor o igual a 80 y menor que 90 , imprimir "B"
+ * -> si es mayor o igual a 70 y menor que 80 , imprimir "C"
+ * -> si es mayor o igual a 60 y menor que 70 , imprimir "D"
+ * -> si es mayor o igual a 50 y menor que 60 , imprimir "E"
+ * -> si es menor 50 , imprimir "F"
+ */
+
+
+/**
+ * 3:
+ * Pedir al usuario por prompt un numero entre 1 y 100
+ * Comparar si ese numero es divisible entre 2, 3 u 11
+ * Imprimir en consola un mensaje que indique de que numero(s) es múltiplo
+ * p.ej. 6 es multiplo de 2 y de 3
+ * p.ej. 22 es multiplo de 2 y de 11
+ * p.ej. 21 es multiplo de 3
+ * Sino, enviar un mensaje en consola que lo indique
+ */
+
+
+/**
+ * 4:
+ * Pedir al usuario por prompt un numero entre 1 y 100
+ * verificar si es un par o impar
+ * imprimir en resultado en consola
+ * Máximo de lineas para este ejercicio, 2 lineas de código
+ * Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+ */
+
+/**
+ * 5:
+ * Pedir al usuario el dia de la semana 
+ * p.ej. lunes, martes, ...
+ * Imprimir en consola el número del dia de la semana equivalente
+ * es decir:
+ * domingo -> 1, lunes -> 2, martes -> 3, etc...
+ */
+
+/**
+ * 6:
+ * Pedir al usuario 2 numeros 
+ * Imprimir en consola cual es el menor de los 2
+ * Imprimir en consola cual es el mayor de los 2
+ */
+
+
+
+
+
+
+
+
+
+
