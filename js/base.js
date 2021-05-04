0649c1e71d8e6f5aaf9835417d5cfad78fb19ea5
dered-dev <jlcc170@gmail.com>
2021-05-03 21:07:23 -0500

    ADD first exercises

--- js/base.js
+++ js/base.js
@@ -1,1 +1,14 @@
-console.log('Hello world')
+console.log('Hola mundo')
+/*
+	Ejercicio 1:
+	Pedir 2 variables por prompt
+	Si a > b dividir a entre b
+	Si a < b dividir sumar a más b
+	si a == b multiplicar ambos numeros
+	Imprimir el resultado en consola
+*/
+
+
+/***
+	Pedir por el promt un numero, y comprobar si es par o impar
+*/
