22ad50b48d38cf52b827157387675296196102a8
dered-dev <jlcc170@gmail.com>
2021-05-05 21:53:40 -0500

    ADD basic exercise while

--- js/base.js
+++ js/base.js
@@ -49,7 +49,10 @@ console.log(stringReversed)
  * 3 x 2 = 6
  */
 
-
+// const numberTable = parseInt(window.prompt('Escribe un numero'))
+// for(let i = 1; i <= 10; i++){
+// 	console.log(`${numberTable} x ${i} = ${numberTable * i}`)
+// }
 
 /**
  * Pedir al usuario una oracion
@@ -58,11 +61,56 @@ console.log(stringReversed)
  * 
  */
 
+// const phrase = window.prompt('Escribe una frase').toLowerCase()
+// let vowels = ''
+// let consonants = ''
+// for(let i = 0; i < phrase.length; i++){
+
+// 	if(phrase[i] !== ' ' || phrase[i] !== ',' || phrase[i] !== '.'){
+// 		if(
+// 			phrase[i] == 'a' || 
+// 			phrase[i] == 'e' || 
+// 			phrase[i] == 'i' || 
+// 			phrase[i] == 'o' || 
+// 			phrase[i] == 'u' || 
+// 			phrase[i] == 'á' || 
+// 			phrase[i] == 'é' || 
+// 			phrase[i] == 'í' || 
+// 			phrase[i] == 'ó' || 
+// 			phrase[i] == 'ú'
+// 		){
+// 			vowels += phrase[i]
+
+// 		} else {
+// 			consonants += phrase[i]
+// 		}
+// 	}
+// }
+// console.log(vowels)
+// console.log(consonants)
+
 /**
  * Pedir el usuario un numero entre 10 y 100
  * Imprimir todos los numeros pares que existen entre 1 y ese numero
  */
 
+// const evenRange = parseInt(window.prompt('Escribe un numero'))
+// for(i=1; i<= evenRange; i++){
+// 	if(i % 2 === 0){
+// 		console.log(i)
+// 	}
+// }
+
+
+// While
+let counter = 1 // inicio
+let fin = 10
+let table = 9 
+while(counter <= 10){ // fin
+	
+	console.log(`${table} x ${counter} =  ${table * counter}`)
+	counter += 1 // aumento / decremento
+}
 
 
 
