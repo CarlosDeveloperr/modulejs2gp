95fe9862c39cbbe83140954a97f5534048e920e6
dered-dev <jlcc170@gmail.com>
2021-05-10 16:57:13 -0500

    Comunnity session one

--- js/base.js
+++ js/base.js
@@ -1,54 +1,109 @@
+// api console
+
+// let valor1 = 'valor uno'
+// let valor2 = 'valor dos'
+// console.log(valor2)
+
+// condicionales
+
+//let expresion = 'otro'
+// let expresion2 = 'otro'
+// let expresion3 = 'otro'
+// if(
+//     (expresion == true || expresion2 != null || expresion3 != undefined) 
+//     && expresion2 != 'perro'
+//     && expresion3 != 'otra cosa'
+// ){
+// //if(expresion === true){
+//   // sentencia
+//   console.log('existo')
+// } else if(expresion === false){
+//   // sentencia
+//   console.log('existo false')
+// } else if(expresion === 'otro'){
+//     // sentencia
+//     console.log('existo otro')
+// } else {
+//     // sentencia
+//     console.log('Default')
+// }
+
+// // operador ternario
+// if(expresion === 'otro'){
+//     console.log('otro')
+// }else {
+//     console.log('desconocido')
+// }
+
+// let expresion = 'otro'
+// expresion === 'otro' ? console.log('otro') : console.log('desconocido')
+
+// let expresion = 'otro'
+// expresion === 'otro' ?(
+//     console.log('otro'),
+//     console.log('otro'),
+//     console.log('otro')
+// ): ''
 
 
-/**
- * For
- * Escribir un programa de arroje la suma de los multiplos de 3, 5 o 7 que hay entre 1 y 100
- * 
- */
 
-let sumMultiples = 0;
-for(i = 1; i <= 100; i++){
-    if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0 ){
-        sumMultiples += i
-    }
-}
+// let valorSwitch = 'oso'
+// switch(valorSwitch){
+//     case 'oso':
+//         console.log('grrrr')
+//     break
+//     case 'perro': 
+//         console.log('guau!')
+//     break
+//     default:
+//         console.log('shh!')
+// }
 
-
- /**
-  * While
-  * Escribir un programa que le pida al usuario un numero
-  * que imprima en consola el numero dado si es positivo
-  * si el numero es negativo, salir del ciclo
-  * 
-  */
-
-let initialNumber = parseInt(window.prompt('Dame el numero inicial'))
-while(initialNumber >= 0){
-    console.log(`el numero introducido fue ${initialNumber}`)
-    initialNumber = parseInt(window.prompt('Dame otro numero'))
-}
+// pedir al usuario su calificacion
+// si la calificacion >= 90
+// imprimir en consola A
+// si la calificacion >= 80
+// imprimir en consola B
+// si la calificacion < 50
+// imprimir en consola F
 
 
-/**
- * Dado los siguientes 2 strings
- * 'smtp'
- * 'aeiou'
- * Imprimir en consola las siguientes combinaciones
- * -> sa, se, si, so , su 
- * -> ma, me, mi, mo , mu 
- * -> ta, te, ti, to , tu 
- * -> pa, pe, pi, po , pu
- */
 
-let consonantStr = 'smtp'
-let vowelsStr = 'aeiou'
+// ciclos
 
-for(let i = 0; i < consonantStr.length; i++) {
-    let strinToPrint = ''
-    for(let j = 0; j < vowelsStr.length; j++){
-        strinToPrint += `${consonantStr[i]}${vowelsStr[j]};`
-    }
-    console.log(strinToPrint)
+// for -> cuando tenemos claro 3 puntos
+// Inicio, fin, aumento
+
+// for(inicio; fin; aumento / decremento)
+
+// while -> cuando tenemos claro 2 puntos
+// inicio , aumento
+// while(condicion)
+
+// let numberUser = window.prompt('Dame un numero') 
+// while(numberUser >= 0){
+//     console.log('numero: '+ numberUser)
+//     numberUser = window.prompt('Dame un numero')  
+// }
+
+// let inicio = 5
+// do{
+//     // 
+//     console.log('Entramos al do while')
+//     inicio++
+//     console.log(inicio)
+// } while(inicio <= 10)
+
+// metodos de string
+
+// Funciones
+
+
+// Encapsular una solucion en un solo bloque 
+
+// good
+function sayHi(nombre, apellido, telefono) {
+    console.log(`How are you, ${nombre}, ${apellido}, ${telefono}?`)
 }
 
 
