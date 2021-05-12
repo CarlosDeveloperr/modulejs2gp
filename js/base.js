16ea88920c23f081a67cff898a716ee3f6f1598a
dered-dev <jlcc170@gmail.com>
2021-05-11 22:48:59 -0500

    ADD array methods && homework

--- js/base.js
+++ js/base.js
@@ -1,1 +1,50 @@
-console.log('Funciones, parametros por defecto, funciones como parametro (callback)')
+console.log('Metodos de array')
+
+let myArray =   [ 'jorge', 8, [1,'luis'] ]
+// console.log( myArray)
+// console.log( myArray[2])
+// console.log( myArray[2][1])
+
+// Métodos de array
+// for(let i = 0;i < myArray.length; i++){
+//     console.log(myArray[i])
+// }
+
+// Metodos funcionales de arrays
+// forEach() -> 
+// reduce() -> unidad
+// filter() -> []
+// map() -> []
+
+// myArray.forEach( function(item, index, arrayOriginal) {
+//     console.log(`indice ${index}:`, item)
+// })
+
+// myArray.forEach( (item, index, arrayOriginal) => {
+//     console.log(`indice ${index}:`, item)
+// })
+
+// const functionCalled = (item, index, arrayOriginal) => {
+//     console.log(`indice ${index}:`, item)
+// }
+
+// myArray.forEach( functionCalled )
+
+// let newArray = []
+// myArray.forEach( (item, index, arrayOriginal) => {
+//     newArray[index] = (index * 100) / 5
+// })
+// console.log(newArray)
+
+// filter method
+let arrayForfilter = [1,2,3,4,5]
+let evensNumbers = arrayForfilter.filter((value, index, array) => {
+    if(value > 3 && value < 5){
+        return value
+    }
+    // return value > 3 && value < 5
+})
+
+// console.log(arrayForfilter)
+// console.log(evensNumbers)
+
--- js/homework.js
+++ js/homework.js
@@ -1,7 +1,24 @@
-/**
- * 1:
- * Investigar Arrow Functions y Métodos de Array
- * 
- */
 
+// Metodos de array
+
+/*
+Escribir una funcion que tome como parametro un array de numeros e imprima en
+consola el array al revés
+arrayReverse([1,2,3,4]). 
+-> [4,3,2,1]
+*/
+
+/*
+Escribir una funcion que tome como parametro 2 arrays, y devuelva un numero 
+con la suma total de esos 2 arrays.
+arrayReduce([1,2,3,4],[1,2]). 
+-> 13
+*/
+
+/*
+Escribir una función que tome como parametro 2 arrays, y devuelva un array 
+con los numeros repetidos en los 2 arrays
+arrayReduce([1,2,3,4],[1,2]). 
+-> [1,2]
+*/
 
