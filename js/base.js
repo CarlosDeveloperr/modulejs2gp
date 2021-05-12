e3fa2b4ebddc84d7b10cb3cfdbb7ea4065f786e7
dered-dev <jlcc170@gmail.com>
2021-05-12 17:34:02 -0500

    ADD exercise class review

--- js/base.js
+++ js/base.js
@@ -1,50 +1,75 @@
 console.log('Metodos de array')
 
-let myArray =   [ 'jorge', 8, [1,'luis'] ]
-// console.log( myArray)
-// console.log( myArray[2])
-// console.log( myArray[2][1])
+// Ejercicio 1 .forEach()
+// Dado un array con solo números
+// obtener el segundo valor mas alto
+// input: [1,4,3,2,5]
+// output: -> 5
 
-// Métodos de array
-// for(let i = 0;i < myArray.length; i++){
-//     console.log(myArray[i])
-// }
 
-// Metodos funcionales de arrays
-// forEach() -> 
-// reduce() -> unidad
-// filter() -> []
-// map() -> []
 
-// myArray.forEach( function(item, index, arrayOriginal) {
-//     console.log(`indice ${index}:`, item)
-// })
+// ejercicio 2 .forEach() o .map() o for()
+// función
+// convertir todos los elementos numericos de un array
+// a strings
+// input: [1,2,3,4,5]
+// output -> ['1,','2','3','4','5']
 
-// myArray.forEach( (item, index, arrayOriginal) => {
-//     console.log(`indice ${index}:`, item)
-// })
 
-// const functionCalled = (item, index, arrayOriginal) => {
-//     console.log(`indice ${index}:`, item)
-// }
 
-// myArray.forEach( functionCalled )
 
-// let newArray = []
-// myArray.forEach( (item, index, arrayOriginal) => {
-//     newArray[index] = (index * 100) / 5
-// })
-// console.log(newArray)
 
-// filter method
-let arrayForfilter = [1,2,3,4,5]
-let evensNumbers = arrayForfilter.filter((value, index, array) => {
-    if(value > 3 && value < 5){
-        return value
-    }
-    // return value > 3 && value < 5
-})
 
-// console.log(arrayForfilter)
-// console.log(evensNumbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Ejercicio 1 .forEach()
+// Dado un array con solo números
+// obtener el segundo valor mas alto
+// input: [1,4,3,2,5]
+// output: -> 5
+
+const mostBiggest  = arr => {
+    let largest = arr[0]
+    arr.forEach(element => {
+        if (largest < element ) {
+            largest = element
+        }
+    })
+    return largest
+}
+
+
+// ejercicio .forEach() o .map() o for()
+// función
+// convertir todos los elementos numericos de un array
+// a strings
+// input: [1,2,3,4,5]
+// output -> ['1,','2','3','4','5']
+
+const numberToStringMap = arr => arr.map( element => element.toString() )
+const numberToStringFe = arr => {
+    let arrStr = []
+    arr.forEach( element => {
+        arrStr.push( element.toString() ) 
+    })
+    return arrStr
+} 
+
 
