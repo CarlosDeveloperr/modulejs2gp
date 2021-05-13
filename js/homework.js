9e4ffe0335affd29cd5cb149a84b9f0469db115d
dered-dev <jlcc170@gmail.com>
2021-05-12 22:41:32 -0500

    ADD Homework exercises forEach && filter

---
+++ js/homework.js
@@ -0,0 +1,40 @@
+/**
+ * 1:
+ * Estudiar los metodos
+ * .map()
+ * .reduce()
+ */
+
+
+/**
+ * Ejericio 1:
+ * Realizar una funcion que dado un array de strings como parametro
+ * retorne un nuevo array con todos los strings que tienen como maximo 5 caracteres
+ */
+
+const fiveCharactersOnly = arr => {
+    // your code here
+}
+
+// test
+let responseFiveCharacters = fiveCharactersOnly(["dog", "wolf", "family", "eaten", "camping"])
+// [ "dog", "wolf", "eaten"]
+
+
+/**
+ * Ejericio 2:
+ * Realizar una funcion que dado un array de nombres de koders como parametro
+ * Retorne un nuevo array con todos koders que tienen mas de 1 nombre
+ * 
+ */
+
+
+ const twoNamesKoders = arr => {
+    // your code here
+}
+
+// test
+let responseFiveCharacters = twoNamesKoders(["Antonio", "Victor Yair", "Luis Carlos", "Victor", "José Omar"])
+// ["Victor Yair", "Luis Carlos", "José Omar"]
+
+
