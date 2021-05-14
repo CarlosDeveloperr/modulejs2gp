3692d41b54be001c858c41f9d4a450cb229aef29
dered-dev <jlcc170@gmail.com>
2021-05-13 23:22:33 -0500

    ADD homework

--- js/base.js
+++ js/base.js
@@ -1,23 +1,41 @@
 console.log('Metodos de array: map y reduce')
 
+
+// forEarch -> nada
+// filter ->  [] con los item que cumplieron la condicion
+// -> puede devolver array vacio
+// .map()  -> array computado 
+// -> tiene la misma longitud del original
+// -> transformar cada item en uno nuevo 
+// 
+
+
+
 // arrFor.map()
 // Ejercicio 1:
 // funcion que dado un array
 // genere un nuevo array con los mismos numeros multiplicados por 2
 function doubleNumbers(arr) {
-    // sentences
+    let response = arr.map( (currentValue, index, array) => {
+        let res = currentValue  * 3
+        res = res + 1
+        return res
+    })
+    return response
 }
-console.log(doubleNumbers([2, 5, 100]));
+let result = doubleNumbers( [2, 5, 100] )
+// console.log(result)
+
 // [4, 10, 200]
+
 
 
 
 // funcion que dado un array
 // genere un nuevo array con los mismos numeros como strings
-function stringItUp(arr) {
-    // sentences
-}
-console.log(stringItUp([2, 5, 100]));
+const stringItUp = arr =>  arr.map(item=> item.toString() )
+
+//console.log(stringItUp([2, 5, 100]));
 // ["2", "5", "100"]
 
 
@@ -25,7 +43,7 @@ console.log(stringItUp([2, 5, 100]));
 function capitalizeNames(arr) {
     // sentences
 }
-console.log(capitalizeNames(["jorge", "antonio", "juan", "victor"]))
+//console.log(capitalizeNames(["jorge", "antOnio", "jUan", "victor"]))
 // ["Jorge", "Antonio", "Juan", "Victor"]
 
 
@@ -35,10 +53,16 @@ console.log(capitalizeNames(["jorge", "antonio", "juan", "victor"]))
 // Crear una funcion que reciba los 2 arrays, 
 // y retorne un array con los nombres completos
 
-function joinNames(arrNames, arrSurnames) {
+const joinNames = (arrNames, arrSurnames) => {
     // sentences
+    return arrNames.map( (value, index) => `${value} ${arrSurnames[index]}`) 
+    // return arrNames.map((value, index) => {
+    //     let fullName = `${value} ${arrSurnames[index]}`
+    //     return fullName
+    // })
 }
-console.log( joinNames( ["jorge", "luis"], ['camarillo', 'cristobal'] ) )
+// const joinNames = (arrNames, arrSurnames) => arrNames.map( (value, index) => `${value} ${arrSurnames[index]}`) 
+// console.log( joinNames( ["jorge", "luis"], ['camarillo', 'cristobal'] ) )
 // ["jorge camarillo ", "luis cristobal"]
 
 
@@ -47,34 +71,49 @@ console.log( joinNames( ["jorge", "luis"], ['camarillo', 'cristobal'] ) )
 
 
 
+
+
+
+
+
 // arrFor.reduce()
-function totalAverage(arr) {
+const totalAverage = (arr) => {
     // sentences
+    let suma = arr.reduce((acumulador , valor, posicion, arr) => {
+        return acumulador + valor
+    }, 600 )
+    return suma
 }
-console.log(totalAverage([1, 2, 3]))
-// 5
 
 
 
-function stringConcat(arr) {
-    // your code here 
-}
-console.log(stringConcat([1, 2, 3])); // "123"
 
 
 
-function totalShoppingCart(arr) {
-    // your code here 
-}
-console.log(totalShoppingCart( [ ["Reloj", 500] , ["Reloj", 500] , ["Reloj", 500] ] ) )
-// 1500
-
-
-function totalVoters(arr) {
-    // your code here 
-}
-console.log(totalVoters( [ ["jorge", false] , ["luis", true] , ["Victor", false] ] ) )
-// 1
+const stringConcat = arr => arr.reduce( (acc,cv) => acc + cv , '') 
+// let resStringconcat = stringConcat([1, 2, 3])
+// console.log(resStringconcat)
 
 
 
+
+// const totalShoppingCart = (arr) => {
+//     return arr.reduce((acc, cv) => {
+//         return acc + cv[1]
+//     }, 0)
+// }
+
+const totalShoppingCart = arr =>  arr.reduce((acc, cv) => acc + cv[1] , 0)
+
+let shopCart = [ 
+        ["Reloj", 1321], 
+        ["Reloj", 3000], 
+        ["Reloj", 1200], 
+        ["Reloj", 1200], 
+        ["Reloj", 1200], 
+        ["Reloj", 1200] 
+    ] 
+
+const totalAPagar = totalShoppingCart(shopCart)
+console.log(`Total a pagar: ${totalAPagar}`)
+// 2000
---
+++ js/homework.js
@@ -0,0 +1,47 @@
+
+/**
+ * 
+ * Ejercicio 1
+ 
+Dado el array [ ["jorge", false] , ["luis", true] , ["Victor", false] ] 
+Calcular el numero de personas que si votaron
+Output -> 1
+Nota: las personas que si votaron tiene true
+
+*/
+
+const totalVoters = (arr) => {
+    // your code here
+}
+
+//console.log(totalVoters( [ ["jorge", false] , ["luis", true] , ["Victor", false] ] ) )
+// -> 1
+
+
+/**
+ * 
+ * Ejercicio 2
+ 
+    Dado un array con pedidos de un restaurant
+    la estructura por cada pedido es 
+    ['entrada','principal','postre','precio'],
+
+    let pedidos = [
+        ['ensalada de pepinos','paella','platano', 100'],
+        ['ensalada de tomates','pescado','helado', 120],
+        ['ensalada simple','paella','yogurt', 80],
+        ['ensalada simple','enchiladas','yogurt', 80],
+        ['ensalada cesar','salmón','platano', 100],
+    ]
+
+    Funcion que devuelve el total de pedidos que tienen como plato principal "paella"
+    Funcion que devuelve los pedidos que tienen como postre "platano" o "helado" 
+    Funcion que devuelve los pedidos que tengen un precio superior a 90 
+    Funcion que devuelve los pedidos que tengen un precio superior a 90 
+
+*/
+
+/**
+ * Ejercicio 3:
+ * Estudiar el tema de Objetos y métodos de Objetos
+ */
